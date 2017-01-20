dffe832b6ed05d371568e0a96b9bd671335700fd
Josiah Gaskin <jcg@fb.com>
2017-01-19 20:16:01 -0800

    Add user lookup

--- axios.js
+++ axios.js
@@ -12,6 +12,16 @@ function post(payload) {
     .catch((error) => error);
 }
 
+function getUser(userId) {
+  // From https://developers.facebook.com/docs/messenger-platform/user-profile
+  // fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=PAGE_ACCESS_TOKEN`
+  const userUrl = `https://graph.facebook.com/v2.6/${userId}?fields=first_name&access_token=${accessToken}`;
+  return axios.get(userUrl)
+    .then((response) => response)
+    .catch((error) => error);
+}
+
 module.exports = {
   post: post,
+  getUser: getUser,
 }
--- messenger.js
+++ messenger.js
@@ -3,27 +3,52 @@
 var fetcher = require('./fetcher');
 var axios = require('./axios');
 
-function reply(entries) {
+function replyToText(messagingItem) {
+  // TODO: call RSS feed based on user input
+  return fetcher('https://www.npr.org/rss/rss.php?id=103537970')
+    .then((feed) => {
+      const story = feed.latestStory();
+      const payload = {
+        recipient: {
+          id: messagingItem.sender.id
+        },
+        message: {
+          text: story.title + ': ' + story.link,
+        }
+      };
+      return axios.post(payload);
+    });
+}
 
+function sayHiAndMenu(messagingItem) {
+  console.log("Saying hi to " + messagingItem.sender.id);
+  return axios.getUser(messagingItem.sender.id)
+    .then(user => {
+      const payload = {
+        recipient: {
+          id: messagingItem.sender.id
+        },
+        message: {
+          text: `Hello ${user.first_name}!`,
+        }
+      };
+      return axios.post(payload);
+    })
+    .catch(error => {
+      console.log("Could not retrieve user for id " + messagingItem.sender.id);
+    });
+}
+
+function reply(entries) {
   // `entries` can contain batches of messages from different users.
   return Promise.all(entries.map((entry) => {
     return Promise.all(entry.messaging.map((messagingItem) => {
-      if (messagingItem.message && messagingItem.message.text) {
-
-        // TODO: call RSS feed based on user input
-        return fetcher('https://www.npr.org/rss/rss.php?id=103537970')
-          .then((feed) => {
-            const story = feed.latestStory();
-            const payload = {
-              recipient: {
-                id: messagingItem.sender.id
-              },
-              message: {
-                text: story.title + ': ' + story.link,
-              }
-            };
-            return axios.post(payload)
-          });
+      if (messagingItem.message) {
+        if (messagingItem.text) {
+          return replyToText(messagingItem);
+        } else {
+          return sayHiAndMenu(messagingItem);
+        }
       } else {
         return Promise.reject('No message sent.');
       }
