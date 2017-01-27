e66c57c3698be2e7f738d0d58a7bd61b9747afcb
Kevin Jennison <kevin.jennison1@gmail.com>
2017-01-26 20:08:44 -0800

    Fix script again. @stage=kevin

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)} 
 COMMIT_MSG=${TRAVIS_COMMIT_MESSAGE}
-STAGE_FROM_COMMIT_MSG=`echo $COMMIT_MSG | sed -nr -e 's/@stage=([a-zA-Z_-]+).*/\1/p'`
+STAGE_FROM_COMMIT_MSG=`echo $COMMIT_MSG | sed -nr -e 's/.*?@stage=([a-zA-Z_-]+).*/\1/p'`
 if [[ -n "$STAGE_FROM_COMMIT_MSG" ]]; then
   STAGE=$STAGE_FROM_COMMIT_MSG
 elif [[ $BRANCH == 'master' ]]; then
