f948b79ed301469d1ff053189025032e970ab53e
Kevin Jennison <kevin.jennison1@gmail.com>
2017-01-26 20:02:44 -0800

    Use stages in deploy. @stage=kevin

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)} 
-if [[ $BRANCH == 'master' ]]; then
+COMMIT_MSG=${TRAVIS_COMMIT_MESSAGE}
+STAGE_FROM_COMMIT_MSG=`echo $COMMIT_MSG | sed -nr -e 's/@stage=([a-zA-Z_-]+).*/\1/p'`
+if [[ -n "$STAGE_FROM_COMMIT_MSG" ]]; then
+  STAGE=$STAGE_FROM_COMMIT_MSG
+fi
+elif [[ $BRANCH == 'master' ]]; then
   STAGE="prod"
 elif [[ $BRANCH == 'develop' ]]; then
   STAGE="dev"
 fi
-if [ -z ${STAGE+x} ]; then
+if [ -z ${STAGE+x} || $STAGE == 'nodeploy' ]; then
   echo "Not deploying changes";
   exit 0;
 fi
